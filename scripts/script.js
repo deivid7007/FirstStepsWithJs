4d9e2418dbdeb4c338f3960fae182417ffa8fc3c
deivid7007 <7deividmladenov007@gmail.com>
2018-04-20 00:07:19 +0300

    Refactoring on architecture.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,13 @@
     var img = $('#floatingImg');
-    var coder = "https://marketing-prod.global.ssl.fastly.net/sites/default/files/2017-03/GA_Coding_2x_2.jpg";
-    var gamer = "http://www.funkykit.com/wp-content/uploads/2017/12/online-gaming.jpg";
 
     $('#coderBtn').click(function () {
         img.css("transform", `rotate(360deg)`);
         img.css("transition", "4s ease-out");
-        img.attr('src', 'coderImg.jpg');
+        img.attr('src', 'img/coderImg.jpg');
     })
 
     $('#gamerBtn').click(function () {
         img.css("transform", `rotate(-360deg)`);
         img.css("transition", "4s ease-out");
-        img.attr('src', 'gamer.jpg');
+        img.attr('src', 'img/gamer.jpg');
     })
